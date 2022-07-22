f6b36cae29d351dbf3965ddfaeef9ad1cddca8c0
Yusuf <Sami.Yusuf5@T-Mobile.com>
2022-07-22 11:57:59 -0700

    added css and js functionality

---
+++ function.js
@@ -0,0 +1,28 @@
+function changeCSS()
+{
+    var defaultCSS = document.getElementById("CSS").href;
+    var CSSLink = defaultCSS.split("/")
+    if(CSSLink.at(-1) == "index.css")
+    {
+        var newCSS = "index1.css"
+        document.getElementById("CSS").href = newCSS;
+    }
+    else if (CSSLink.at(-1) == "index1.css"){
+        var newCSS = "index.css"
+        document.getElementById("CSS").href = newCSS;
+    }
+    localStorage.setItem("currCSS", newCSS);
+}
+
+function setDefaultCSS()
+{
+    var defaultCSS = localStorage.getItem("currCSS");
+    if(!defaultCSS){
+        localStorage.setItem("currCSS", "index1.css");
+    }
+}
+
+window.onload = function(){
+    setDefaultCSS();
+    document.getElementById("CSS").href = localStorage.getItem("currCSS");
+}
